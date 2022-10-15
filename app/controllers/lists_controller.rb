4c2e8fc887153ae80e94ba242365f0e553bf0d15
KubotaRikuto <114932185+KubotaRikuto@users.noreply.github.com>
2022-10-15 18:36:22 +0000

    [Finish] 3-3

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,33 @@
 class ListsController < ApplicationController
   def new
+    # Viewへ渡すためのインスタント変数に空のModuleオブジェクトを生成する。
+    @list = List.new
+  end
+
+  # 以下追加
+  def create
+    #データを受け取り新規登録するためのインスタンスを作成
+    list = List.new(list_params)
+    # データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # トップ画面へリダイレクト
+    redirect_to '/top'
   end
 
   def index
+    @lists = List.all
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  # ストロングパラメータ
+  # list_params は保存したいデータの絞り込みを行う。
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
