5af09453c52064fbb3f2cf834e20a6245db2e289
KubotaRikuto <114932185+KubotaRikuto@users.noreply.github.com>
2022-10-17 10:00:59 +0000

    [Finish] 3-5 編集機能を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   get 'lists/new'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/top' => 'homes#top'
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
   #  .../lists/1 や .../lists/3に該当　(1,3 はidのこと)
   get 'lists/:id' => 'lists#show' , as: 'list'
-  get 'lists/edit'
+  get 'lists/:id/edit' => 'lists#edit' , as: 'edit_list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
