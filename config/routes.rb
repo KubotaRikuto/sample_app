04550dd7ffc25e57ebead84f20bcf37f510bec4c
KubotaRikuto <114932185+KubotaRikuto@users.noreply.github.com>
2022-10-18 09:22:58 +0000

    [Finish] 3-7 削除機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  get 'lists/new'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/top' => 'homes#top'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  #  .../lists/1 や .../lists/3に該当　(1,3 はidのこと)
-  get 'lists/:id' => 'lists#show' , as: 'list'
-  get 'lists/:id/edit' => 'lists#edit' , as: 'edit_list'
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
+  resources :lists #listsコントローラに紐づく以下のルーティングを自動生成
+    # new(投稿を作成する画面)
+    # show(投稿の詳細画面)
+    # index(投稿の一覧画面)
+    # edit(投稿の編集画面)
+    # create(投稿作成)
+    # destroy(投稿削除)
+    # update(投稿更新)
 
 end
